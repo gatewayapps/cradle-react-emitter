f150dce67d14286a3306164975c68c8bcb5e57c8
Daniel Gary <daniel@gatewayapps.com>
2018-09-20 17:06:31 -0500

    removed dist folder

--- dist/IReactEmitterOptions.d.ts
+++
@@ -1,8 +0,0 @@
-import { CradleModel, PropertyTypes } from '@gatewayapps/cradle';
-import { ReactComponentTypes } from './ReactEmitter';
-export interface IReactEmitterOptions {
-    readonly getOutputPath: (modelName: string, componentType: typeof PropertyTypes.PropertyType) => string;
-    readonly renderPropertyForComponentType: (modelName: string, propertyName: string, propertyType: typeof PropertyTypes.PropertyType, componentType: ReactComponentTypes) => string;
-    readonly renderModelForComponentType: (model: CradleModel, componentType: ReactComponentTypes, renderedProperties: string[]) => string;
-}
-//# sourceMappingURL=IReactEmitterOptions.d.ts.map
