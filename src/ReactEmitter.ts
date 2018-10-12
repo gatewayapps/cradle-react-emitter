81560c714687ad30db08c31f9383ce4653cb7584
Daniel Gary <daniel@gatewayapps.com>
2018-10-12 09:07:27 -0500

    refactored emitter options and added support for model references

--- src/ReactEmitter.ts
+++ src/ReactEmitter.ts
@@ -15,7 +15,7 @@ export enum ReactComponentTypes {
 
 export default class ReactEmitter implements ICradleEmitter {
   public console?: IConsole
-  public options?: EmitterOptions<IReactEmitterOptions>
+  public options!: EmitterOptions<IReactEmitterOptions>
 
   public async prepareEmitter(options: EmitterOptions<IReactEmitterOptions>, console: IConsole) {
     this.console = console
@@ -42,23 +42,28 @@ export default class ReactEmitter implements ICradleEmitter {
       if (selectFile) {
         emittedFiles.push(selectFile.toString())
       }
+
+      const otherFiles: string[] = []
+      this.options.options.additionalComponentTypes!.forEach((ct) => {
+        const rendered = this.emitModelForComponentType(m, ct)
+      })
     })
-    if (this.options!.options.onComplete) {
-      this.options!.options.onComplete(emittedFiles)
+    if (this.options.options.onComplete) {
+      this.options.options.onComplete(emittedFiles)
     }
   }
 
-  private emitModelForComponentType(model: CradleModel, componentType: ReactComponentTypes): string | boolean {
-    if (this.options!.options.shouldRenderModelForComponentType) {
-      if (this.options!.options.shouldRenderModelForComponentType!(model, componentType) === false) {
+  private emitModelForComponentType(model: CradleModel, componentType: ReactComponentTypes | string): string | boolean {
+    if (this.options.options.shouldRenderModelForComponentType) {
+      if (this.options.options.shouldRenderModelForComponentType!(model, componentType) === false) {
         this.console!.debug(colors.red(`Skipping ${model.Name}:${componentType}`))
         return false
       }
     }
 
-    const fileName = this.options!.options.getOutputPath(model.Name, componentType)
+    const fileName = this.options.options.getOutputPathForComponentType(model.Name, componentType)
     const absoluteFilename = path.resolve(fileName)
-    if (existsSync(absoluteFilename) && !this.options!.options.overwriteExisting) {
+    if (existsSync(absoluteFilename) && !this.options.options.overwriteExisting) {
       this.console!.debug(colors.red(`Skipping ${absoluteFilename} as it already exists`))
       return false
     }
@@ -66,33 +71,37 @@ export default class ReactEmitter implements ICradleEmitter {
 
     const absoluteDir = path.dirname(absoluteFilename)
     ensureDirSync(absoluteDir)
+    const renderedChildren: Record<string, string | undefined> = {}
+    if (this.options.options.renderPropertyForComponentType) {
+      const propertyNames = Object.keys(model.Properties)
+      propertyNames.forEach((name) => {
+        const property = model.Properties[name]
+        if (
+          this.options.options.shouldRenderPropertyForComponentType === undefined ||
+          this.options.options.shouldRenderPropertyForComponentType!(model, name, property, componentType)
+        ) {
+          renderedChildren[name] = this.options.options.renderPropertyForComponentType!(model, name, property, componentType)
+        }
+      })
+    }
 
-    const renderedProperties: string[] = []
-    const propertyNames = Object.keys(model.Properties)
-    propertyNames.forEach((name) => {
-      const property = model.Properties[name]
-      if (!this.options!.options.shouldRenderPropertyForComponentType || this.options!.options.shouldRenderPropertyForComponentType!(model, name, property, componentType)) {
-        renderedProperties.push(this.renderPropertyForComponentType(name, property, componentType, model))
-      }
-    })
+    if (!!this.options.options.renderReferenceForComponentType) {
+      const referenceNames = Object.keys(model.References)
+      referenceNames.forEach((name) => {
+        const reference = model.References.get(name)
+        if (reference !== undefined) {
+          if (
+            this.options.options.shouldRenderReferenceForComponentType === undefined ||
+            this.options.options.shouldRenderReferenceForComponentType!(model, name, reference, componentType)
+          ) {
+            renderedChildren[name] = this.options.options.renderReferenceForComponentType!(model, name, reference, componentType)
+          }
+        }
+      })
+    }
 
-    const fileContents = this.options!.options.renderModelForComponentType(model, componentType, renderedProperties)
+    const fileContents = this.options.options.renderModelForComponentType(model, componentType, renderedChildren)
     writeFileSync(absoluteFilename, fileContents, 'utf8')
     return absoluteFilename
-  }
-
-  private renderPropertyForComponentType(propertyName, property: PropertyType, componentType: ReactComponentTypes, model: CradleModel): string {
-    switch (componentType) {
-      case ReactComponentTypes.Detail:
-        return this.options!.options.renderDetailProperty(propertyName, property, model)
-      case ReactComponentTypes.Edit:
-        return this.options!.options.renderEditProperty(propertyName, property, model)
-      case ReactComponentTypes.List:
-        return this.options!.options.renderListProperty(propertyName, property, model)
-      case ReactComponentTypes.Select:
-        return this.options!.options.renderSelectProperty(propertyName, property, model)
-      default:
-        return 'NOT SUPPORTED'
-    }
   }
 }
